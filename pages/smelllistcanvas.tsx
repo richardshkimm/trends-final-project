2ad52a02619d52a88c78898383b01d90dae1b5ce
Richard Kim <rk625@cornell.edu>
2022-04-29 16:21:33 -0400

    Add changes to .gitignore and added page

--- pages/smelllistcanvas.tsx
+++ pages/smelllistcanvas.tsx
@@ -1,5 +1,12 @@
 export default function SmellListCanvas() {
     return (
-        <h1>This is the smell list canvas</h1>
+        <div>
+        <h1> Smells Reported </h1>
+        <p> 1.  </p>
+        <p> 2.  </p>
+        <p> 3.  </p>
+        <p> 4.  </p>
+        <p> 5.  </p>
+        </div>
     );
   }
