fc1f86bc331ccfc3617e78b07ba6b79ec4ec39af
Benjamin Shapiro <bps78@cornell.edu>
2022-04-30 16:48:22 -0400

    updating overlay

--- pages/mapcanvas.tsx
+++ pages/mapcanvas.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 import { Map, Marker, Overlay } from "pigeon-maps"
+import CancelIcon from '@mui/icons-material/Cancel';
+import Box from '@mui/material/Box'
 
 export default function MapCanvas() {
     const [hue, setHue] = useState(0)
@@ -14,16 +16,31 @@ export default function MapCanvas() {
         }
         else{
             return(
-                <Overlay anchor={[overlayLatLng[0], overlayLatLng[1]]} offset={[120, 79]}>
-                    <img src='https://www.engineering.cornell.edu/sites/default/files/content/faculty/image/david-gries-NEW.jpg' width={240} height={158} alt='' />
+                <Overlay anchor={[overlayLatLng[0], overlayLatLng[1]]} offset={[75, 75]}>
+                    <Box
+                        sx={{
+                        width: 150,
+                        height: 150,
+                        backgroundColor: 'gray',
+                        borderRadius: '30%',
+                        }}>
+                            <h3>place a smell?</h3>
+                            <CancelIcon color="primary"/>
+                  
+                  
+                    </Box>
+                    
+                    
                 </Overlay>)
     }
 }
 
+//offset should be half the height and width of the size of the overlay element (both positive)
     return (
         <div>
             <Map height="99.7vh" defaultCenter={[42.444, -76.48]} defaultZoom={15} maxZoom={19} onClick={({event, latLng, pixel}) => setOverlayLatLng([latLng[0],latLng[1]])}>
                {overlayDisplay(overlayLatLng)}
+               
             </Map>
         </div>
     );
