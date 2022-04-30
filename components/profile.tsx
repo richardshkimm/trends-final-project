4db792a6b3ff53f4cf5f8649c70b8a790be6228d
Sanjana Kasetti <sk2465@cornell.edu>
2022-04-30 19:11:48 -0400

    modfied profile.tsx, profilecanvas.tsx, profile.module.css

--- components/profile.tsx
+++ components/profile.tsx
@@ -5,24 +5,44 @@ import profile from "../styles/profile.module.css"
 import { deepOrange, deepPurple } from '@mui/material/colors';
 import Button from '@mui/material/Button';
 
+
 export default function LetterAvatars() {
   return (
+    
     <div className={profile.container}>
-        {/*<Stack spacing={2} direction="row">*/}
+        <Stack spacing={2} direction="row">
         <div className={profile.avatar}>
-            <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
+            <Avatar sx={{ bgcolor: deepOrange[500] }}>N
+
+            </Avatar>
          </div>    
 
-        {/*</Stack>*/}
-        <div>
+        </Stack>
+        <div className={profile.details}>
+            <div className={profile.posts}>
+                <h1>
+                    28   
+                    <small> Posts</small>
+                </h1>
+            </div>
+            <div className={profile.Followers}>
+                <h1>
+                    10
+                    <small> Followers </small>
+                </h1>
+            </div>
             <h1>
-                Followers
+                29
+                <small> Following</small>
             </h1>
-        </div>    
 
-        <Button variant="contained" disableElevation>
-            Follow    
+        </div>    
+        <div className = {profile.follow}>
+            <Button variant="contained" disableElevation>
+                
+                Follow    
         </Button>
+        </div>
     </div>
 
     
