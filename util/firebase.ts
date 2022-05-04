308a379605a9234856753062a50664f4940a47a7
Richard Kim <richardshkim04@gmail.com>
2022-05-04 18:10:39 -0400

    Add firebase

---
+++ util/firebase.ts
@@ -0,0 +1,18 @@
+// npm install firebase
+import { initializeApp, getApps, getApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTau5mXiVX0iIXRab31-dk7Vk6bT42wEg",
+  authDomain: "aromap-42052.firebaseapp.com",
+  projectId: "aromap-42052",
+  storageBucket: "aromap-42052.appspot.com",
+  messagingSenderId: "442786158561",
+  appId: "1:442786158561:web:0ba1da5f1f7444619724a0"
+}
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export { db }
