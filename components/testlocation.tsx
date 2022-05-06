212e3b47f9d91377bbf756fc33128b94ee516772
Sanjana Kasetti <sk2465@cornell.edu>
2022-05-05 22:03:38 -0400

    added testlocation.tsx, which gets the user's location and modified profilecanvas.tsx

---
+++ components/testlocation.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react"
+import { Map, Marker, Overlay } from "pigeon-maps"
+import CancelIcon from '@mui/icons-material/Cancel';
+import Box from '@mui/material/Box'
+import Smell from '../components/smell';
+import styles from '../styles/bigmap.module.css'
+import Button from '@mui/material/Button';
+
+export default function MapCanvas() {
+
+    const [stat, setStat] = useState<String>("");
+    const [lat, setLat] = useState<number>(0);
+    const [long, setLong] = useState<number>(0);    
+
+    const getLocation = () => {
+        if (!navigator.geolocation){
+            setStat("Retrieving your Location is not supported by your broswer")
+        }
+        const success = (pos : GeolocationPosition) => {
+            setStat("");
+            setLong(pos.coords.longitude);
+            setLat(pos.coords.latitude);
+        }
+
+        const error = (error: GeolocationPositionError) => {
+            setStat("Location unretrievable")
+        }
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+
+//offset should be half the height and width of the size of the overlay element (both positive)
+    return (
+            <div> 
+              <button onClick={getLocation}>Get Location</button>
+              <h1>Coordinates</h1>
+              <p>{stat}</p>
+              {lat && <p>Latitude: {lat}</p>}
+              {long && <p>Longitude: {long}</p>}
+            </div>
+          );
+}
--- pages/profilecanvas.tsx
+++ pages/profilecanvas.tsx
@@ -1,7 +1,10 @@
-import Component from "../components/profile"
+import Profile from "../components/profile"
+import Component from "../components/testlocation"
+
 export default function ProfileCanvas() {
     return (
        // <h1>This is the profile canvas</h1>
         <Component />
+
     );
   }
